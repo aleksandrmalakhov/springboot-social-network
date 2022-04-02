9a7872f761c3f0d0c9920a546175511af63b118a
Aleksandr Malakhov <schwarcroza@mail.ru>
2022-04-02 15:22:09 +0300

    initializing the repository

---
+++ src/main/java/com/example/springboot_social_network/SpringbootSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_social_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSocialNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_social_network/SpringbootSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_social_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
