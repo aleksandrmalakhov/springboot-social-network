bd7399622fe0e08c7112c139ee8173b6754ca0e2
Aleksandr Malakhov <schwarcroza@mail.ru>
2022-04-04 20:24:30 +0300

    Add hasAuthority

--- src/main/java/com/example/springboot_social_network/entity/Role.java
+++ src/main/java/com/example/springboot_social_network/entity/Role.java
@@ -3,7 +3,8 @@ package com.example.springboot_social_network.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER,
+    ADMIN;
 
     @Override
     public String getAuthority() {
